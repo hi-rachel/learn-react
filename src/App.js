6696884f56060f110a4b4a8854460e10df12bc61
rachel <rachel.uiux@gmail.com>
2023-12-02 17:28:49 +0900

    19.2.2 React.lazy와 Suspense 사용하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react";
+import { useState, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+const SplitMe = React.lazy(() => import("./components/SplitMe"));
 
 function App() {
+  const [visible, setVisible] = useState(false);
   const onClick = () => {
-    import("./notify").then((result) => result.default());
+    setVisible(true);
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p onClick={onClick}>Hello React</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
       </header>
     </div>
   );
